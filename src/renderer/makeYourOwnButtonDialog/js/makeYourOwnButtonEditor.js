16d430d17424fe6e849a6f89bd1c0e3da4bf75d0
Javier Hernández <javi@raisingthefloor.org>
2021-03-17 13:23:45 +0100

    GPII-4173: Added selectors and message distribution of the MYOB editor

--- src/renderer/makeYourOwnButtonDialog/js/makeYourOwnButtonEditor.js
+++ src/renderer/makeYourOwnButtonDialog/js/makeYourOwnButtonEditor.js
@@ -22,7 +22,150 @@
      * Represents the controller for the settings editor.
      */
     fluid.defaults("gpii.makeYourOwnButtonEditor", {
-        gradeNames: ["fluid.viewComponent"]
+        gradeNames: ["fluid.viewComponent", "gpii.binder.bindOnCreate"],
+
+        model: {
+            // TODO: null all the messages and move them into the corresponding
+            // message bundle.
+            messages: {
+                // button name
+                nameLabel: "Name",
+                namePlaceholder: "Enter name for button",
+
+                // button type
+                typeLabel: "Button Type",
+                typeOptionWeb: "Button that opens a website",
+                typeOptionApp: "Button that opens an application",
+
+                // button data
+                dataLabelWeb: "URL Address",
+                dataLabelApp: "Path to application executable",
+                dataPlaceholderWeb: "Enter address of the webpage",
+                dataPlaceholderApp: "Enter path to application executable",
+
+                // button popup
+                popupLabel: "Popup Text",
+                popupPlaceholder: "Enter Popup Text",
+
+                // button description
+                descriptionLabel: "Description",
+                descriptionPlaceholder: "Enter description",
+
+                // save button
+                saveButtonLabel: "Save button"
+            },
+
+            buttonDef: {
+                buttonId: "MakeYourOwn",
+                buttonName: null,
+                buttonType: null,
+                buttonData: null,
+                popupText: null,
+                description: null,
+                fullScreen: null // TODO: Add into UI
+            }
+        },
+
+        selectors: {
+            // label of the preview button
+            btnPreviewLabel:     ".flc-myobEditor-btnPreviewLabel",
+
+            // button name
+            nameLabel:          ".flc-myobEditor-nameLabel",
+            nameInput:          ".flc-myobEditor-nameInput",
+
+            // button type
+            typeLabel:          ".flc-myobEditor-typeLabel",
+            typeInput:          ".flc-myobEditor-typeInput",
+            typeOptionWeb:      ".flc-myobEditor-typeOptionWeb",
+            typeOptionApp:      ".flc-myobEditor-typeOptionApp",
+
+            // button data
+            // * can be either WEB or APP
+            // * labels needs to be updated when the selection changes
+            dataLabel:          ".flc-myobEditor-dataLabel",
+            dataInput:          ".flc-myobEditor-dataInput",
+
+            // button popup text
+            popupLabel:         ".flc-myobEditor-popupLabel",
+            popupInput:         ".flc-myobEditor-popupInput",
+
+            // button description
+            descriptionLabel:   ".flc-myobEditor-descriptionLabel",
+            descriptionInput:   ".flc-myobEditor-descriptionInput",
+
+            // save button
+            saveButton:         ".flc-myobEditor-saveButton"
+        },
+
+        bindings: {
+            // selector : model
+        },
+
+        modelListeners: {
+            // UI message initialization with i18n support
+            "messages.nameLabel": {
+                this: "{that}.dom.nameLabel",
+                method: "text",
+                args: "{that}.model.messages.nameLabel"
+            },
+            "messages.namePlaceholder": {
+                this: "{that}.dom.nameInput",
+                method: "attr",
+                args: ["placeholder", "{that}.model.messages.namePlaceholder"]
+            },
+            "messages.typeLabel": {
+                this: "{that}.dom.typeLabel",
+                method: "text",
+                args: "{that}.model.messages.typeLabel"
+            },
+            "messages.typeOptionWeb": {
+                this: "{that}.dom.typeOptionWeb",
+                method: "text",
+                args: "{that}.model.messages.typeOptionWeb"
+            },
+            "messages.typeOptionApp": {
+                this: "{that}.dom.typeOptionApp",
+                method: "text",
+                args: "{that}.model.messages.typeOptionApp"
+            },
+            "messages.dataLabelWeb": {
+                this: "{that}.dom.dataLabel",
+                method: "text",
+                args: "{that}.model.messages.dataLabelWeb"
+            },
+            "messages.dataPlaceholderWeb": {
+                this: "{that}.dom.dataInput",
+                method: "attr",
+                args: ["placeholder", "{that}.model.messages.dataPlaceholderWeb"]
+            },
+            "messages.popupLabel": {
+                this: "{that}.dom.popupLabel",
+                method: "text",
+                args: "{that}.model.messages.popupLabel"
+            },
+            "messages.popupPlaceholder": {
+                this: "{that}.dom.popupInput",
+                method: "attr",
+                args: ["placeholder", "{that}.model.messages.popupPlaceholder"]
+            },
+            "messages.descriptionLabel": {
+                this: "{that}.dom.descriptionLabel",
+                method: "text",
+                args: "{that}.model.messages.descriptionLabel"
+            },
+            "messages.descriptionPlaceholder": {
+                this: "{that}.dom.descriptionInput",
+                method: "attr",
+                args: ["placeholder", "{that}.model.messages.descriptionPlaceholder"]
+            },
+            "messages.saveButtonLabel": {
+                this: "{that}.dom.saveButton",
+                method: "text",
+                args: "{that}.model.messages.saveButtonLabel"
+            }
+        }
+
     });
 
 })(fluid);
